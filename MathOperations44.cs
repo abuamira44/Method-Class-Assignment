cea8bd78a0ed4387d8a6fcab60d227994d9ea087
abuamira44 <145227273+abuamira44@users.noreply.github.com>
2024-01-26 14:41:35 -0500

    Add files via upload
    comiit

---
+++ MathOperations44.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    internal class MathOperations
+    {
+        // MathOperations.cs
+
+
+
+    
+        // Create a void method that takes two integers as parameters
+        public void PerformMathOperation(int firstNumber, int secondNumber)
+        {
+            // Do a math operation on the first integer (e.g., add 10)
+            int result = firstNumber + 10;
+
+            // Display the result of the math operation and the second integer to the screen
+            Console.WriteLine($"Result of {firstNumber} + 10 = {result}. Displaying second integer: {secondNumber}");
+        }
+    }
+
+}
+
